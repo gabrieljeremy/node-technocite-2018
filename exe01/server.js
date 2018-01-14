f2b91a911668e7e19147bf6577c3e13ec6eeed0a
gabrieljeremy <gabriel.jeremy21@gmail.com>
2018-01-14 19:30:52 +0100

    maj ex

--- exe01/server.js
+++ exe01/server.js
@@ -1,41 +1,41 @@
-// console.log(0.1+0.2) // erreur de la virgule flotante : faut l'arrondir
+console.log(0.1+0.2) // erreur de la virgule flotante : faut l'arrondir
 
-// test() // fonctionne car le javascript est lu une 1ere fois et remonte toutes les fonctions
-// var a = 3
-// function test() {
-//     var a = 5 // n'existe que dans cette fonction
-// }
-// console.log(a)
+test() // fonctionne car le javascript est lu une 1ere fois et remonte toutes les fonctions
+var a = 3
+function test() {
+    var a = 5 // n'existe que dans cette fonction
+}
+console.log(a)
 
-// const a = 3
-// a = 5 // on ne peut pas modifier la constante
-// const arr = []
-// arr.push('test') // on peut par contre modifier un tableau
-// arr[1] = 'test2'
+const a = 3
+a = 5 // on ne peut pas modifier la constante
+const arr = []
+arr.push('test') // on peut par contre modifier un tableau
+arr[1] = 'test2'
 
-// let a = 4
-// { // blocking scope : englobe la variable a dans un scope spécifique (utilisable que dans les accolades)
-//     let a = 3
-// }
-// console.log(a)
+let a = 4
+{ // blocking scope : englobe la variable a dans un scope spécifique (utilisable que dans les accolades)
+    let a = 3
+}
+console.log(a)
 
-// (function(msg) { // fonction anonyme basée sur une closure => ('a') execute la fonction elle-même et passe le a en paramètre de la fonction
-//     console.log(msg)
-// })('a')
+(function(msg) { // fonction anonyme basée sur une closure => ('a') execute la fonction elle-même et passe le a en paramètre de la fonction
+    console.log(msg)
+})('a')
 
-// var obj = {
-//     name : 'gilles',
-//     job : 'student',
-//     toString : function() {
-//         return `name is ${this.name} job is ${this.job}` // magic string
-//     }
-// }
-// console.log(obj.toString())
+var obj = {
+    name : 'gilles',
+    job : 'student',
+    toString : function() {
+        return `name is ${this.name} job is ${this.job}` // magic string
+    }
+}
+console.log(obj.toString())
 
-// const arr = [a,b] = ['gilles', 'bertrand'] // déclarer plusieurs variables dans un array
-// console.log(b)
-// console.log(a)
-// console.log(arr)
+const arr = [a,b] = ['gilles', 'bertrand'] // déclarer plusieurs variables dans un array
+console.log(b)
+console.log(a)
+console.log(arr)
 
 // EXERCICES
 
@@ -43,39 +43,40 @@ const numbers = [1,2,3,4,5,6,7,8]
 
 // Doubler les nombres compris dans ce tableau
 
-// function dblNumbers(arr) {
+function dblNumbers(arr) {
     
-//     // const arrDbl = []
-//     // for (let i = 0, length = arr.length; i<length; i++) { // length est calculé qu'une seule fois à cause qu'on l'initialise dans la boucle for
-//     //     arrDbl.push(arr[i]*2)
-//     // }
-//     // return arrDbl
+    // const arrDbl = []
+    // for (let i = 0, length = arr.length; i<length; i++) { // length est calculé qu'une seule fois à cause qu'on l'initialise dans la boucle for
+    //     arrDbl.push(arr[i]*2)
+    // }
+    // return arrDbl
 
-//     // const arrDbl = []
-//     // arr.map(number => arrDbl.push(number*2))
-//     // return arrDbl
+    // const arrDbl = []
+    // arr.map(number => arrDbl.push(number*2))
+    // return arrDbl
 
-//     // return arr.map(function(item) {
-//     //     return item*2
-//     // })
+    // return arr.map(function(item) {
+    //     return item*2
+    // })
 
-//     return arr.map(item => item*2) // le return n'est pas obligatorie
-// }
-// console.log(dblNumbers(numbers))
+    return arr.map(item => item*2) // le return n'est pas obligatorie
+
+}
+console.log(dblNumbers(numbers))
 
 // Additionner tous les nombres
-// console.log(numbers.reduce((prev, current) => { // prev = somme des nombres précédents et current = nombre à additionner ensuite
-//     return prev + current
-// }, 0)) // la première valeur à utiliser
+console.log(numbers.reduce((prev, current) => { // prev = somme des nombres précédents et current = nombre à additionner ensuite
+    return prev + current
+}, 0)) // la première valeur à utiliser
 
 // Construire un nouvel array avec des strings
-// console.log(numbers.reduce((prev, current) => { // prev = somme des nombres précédents et current = nombre à additionner ensuite
-//     prev.push(current.toString() + 'tttt')
-//     return prev
-// }, [])) // la première valeur à utiliser
+console.log(numbers.reduce((prev, current) => { // prev = somme des nombres précédents et current = nombre à additionner ensuite
+    prev.push(current.toString() + 'tttt')
+    return prev
+}, [])) // la première valeur à utiliser
 
 // Afficher les numéros pairs
-// console.log(numbers.filter(item => item % 2 === 0))
+console.log(numbers.filter(item => item % 2 === 0))
 
 // Afficher les nombres impairs
 console.log(numbers.filter(item => item % 2 !== 0))
--- exe02/server.js
+++ exe02/server.js
@@ -1,4 +1,4 @@
-const [action, value] = [process.argv[2], process.argv[3]] // Mise de la 3ème et de la 4ème valeur dans une variable
+const [action, value] = [process.argv[2], process.argv[3]] // Mise de la 3ème et de la 4ème valeur du terminal dans une variable
 const possibleActions = ['add', 'remove'] // Stock des actions possibles dans une constante qui peut changer
 const fileMger = require('./ext/fileManager') // On importe un fichier (. => dans ce dossier)
 
@@ -33,58 +33,16 @@ const init = () => { // Fonction qui initialise l'application
     } else {
         fileMger.init('liste.txt',action, value) // On passe le fichier, l'action et la valeur
     }
-    // } else if (action === 'add') {
-    //     names.push(value)
-    //     fs.writeFile(`${process.cwd()}/liste.txt`, 'Hello Node.JS', (err) => {
-    //         if (err) throw err;
-    //         console.log('The file has been saved!');
-    //     });
-    // }
 }
 init() // On lance la fonction
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// récupère les valeurs entrées dans le terminal
-// console.log(process.argv)
-
-// function add () {
-
-// }
-
-// fs.readFile(`${process.cwd()}/liste.txt`, {encoding :'utf8'}, (err, data) => {
-//     if (err) throw err // si tu rencontres une erreur, tu lances l'erreur et stoppe le serveur ---- ou console.log(err)
-//     names = data.toString().split('\n')
-//     init()
-//     console.log(data)
-// })
-
-
-
-
 // fs.readFile(`${process.cwd()}/liste.txt`, {encoding :'utf8'}, (err, data) => {
 //     if (err) throw err // si tu rencontres une erreur, tu lances l'erreur et stoppe le serveur ---- ou console.log(err)
 //     console.log(data)
 // })
+
+
 // console.log(__dirname) // donne moi le répertoire dans lequel se trouve ce fichier ci
 // console.log(process.cwd()) // donne moi le répertoire où Node s'exécute actuellement
 
